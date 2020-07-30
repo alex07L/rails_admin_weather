3ecddb594ee46c3a0665f952ef63c708b199b0c4
alex07L <alexlevesque7@hotmail.fr>
2020-07-30 15:14:48 -0400

    fix weather tab

--- lib/rails_admin_weather/rails_admin/config/actions/weather.rb
+++ lib/rails_admin_weather/rails_admin/config/actions/weather.rb
@@ -4,7 +4,7 @@ module RailsAdmin
       class Weather < RailsAdmin::Config::Actions::Base
         RailsAdmin::Config::Actions.register(self)
 
-        register_instance_option :collection? do
+        register_instance_option :member do
           true
         end
 
